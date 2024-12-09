002a9b54026efa9c8c17033559be859b359d948d
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2024-12-09 10:03:03 -0500

    code complete

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,17 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+
+filehandle=open("question.txt","w")
+m = str(input("please enter a question:  "))
+a = str(input("Please enter option a:  "))
+b = str(input("Please enter option b:  "))
+c = str(input("Please enter option c:  "))
+d = str(input("Please enter option d:  "))
+x = str(input("Please enter the correct answer:  "))
+filehandle.write(m)
+filehandle.write ("a." + a)
+filehandle.write ("b)" + b)
+filehandle.write ("c)" + c)
+filehandle.write ("d)" + d)
+filehandle.write ("Correct answer" + x)
+filehandle.close()
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,4 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+filehandle=open("question.txt","w")
+filehandle.close()
