63e7d87239cd0fefea84fafde40c3f90eea36e7d
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2024-12-10 10:23:35 -0500

    DONE

--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,8 +1,9 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
-filehandle = open("question.txt","r")
-s = 0
-print ("Quiz")
+filehandle = open("question.txt", "r")
+
+print("Quiz")
+
 while True:
     question = filehandle.readline().strip()
     if not question:
@@ -11,21 +12,20 @@ while True:
     b = filehandle.readline().strip()
     c = filehandle.readline().strip()
     d = filehandle.readline().strip()
-    x = filehandle.readline().strip().split(": ")[1]
-    print (question)
-    print (a)
-    print (b)
-    print (c)
-    print (d)
-    y = input("Enter your answer")
+    x = filehandle.readline().strip()
+
+    print(question)
+    print("a.", a)
+    print("b.", b)
+    print("c.", c)
+    print("d.", d)
+
+    y = input("Enter your answer: ")
     if y == x:
-        print ("You are right")
-        s = s + 1
+        print("You are right")
     else:
-        print ("You are wrong. The correct answer was", x)
-        
-    
-    
-    
+        print("You are wrong. The correct answer was:", x)
+
 filehandle.close()
-print ("Good Job :)", s)
+
+print("Good Job :)")
